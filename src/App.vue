62e30e2826fd774639ba6bfa6eea6a66fb969aae
Gerwin Overeem <gerwin@overeem.io>
2019-01-12 20:05:26 +0100

    Landing page created

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,64 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <vue-typed-js :strings="['Gerwin en Sophie']" :showCursor="false">
+      <h1 class="typing"></h1>
+    </vue-typed-js>
+    <vue-typed-js :strings="['22 juni 2019']" :startDelay="2000" :showCursor="false">
+      <h2 class="typing"></h2>
+    </vue-typed-js>
+    <vue-typed-js :strings="['Informatie volgt...']" :startDelay="3500" :showCursor="false">
+      <h3 class="typing"></h3>
+    </vue-typed-js>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+//import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'app',
-  components: {
-    HelloWorld
-  }
+  // components: {
+  //   HelloWorld
+  // }
 }
 </script>
 
 <style>
+@media only screen and (min-width: 600px) {
+  h1 {
+    font-size: 80px;
+    margin-bottom: 0px;
+  }
+
+  h2 {
+    font-size: 60px;
+  }
+
+  h3 {
+    font-size: 30px;
+  }
+}
+
+h2 {
+  font-weight: 400;
+}
+
+h3 {
+  margin-top: 100px;
+  font-style: italic;
+  font-weight: 200;
+}
+
+.typed-element {
+  justify-content: center;
+}
+
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+  font-family: 'Montserrat', Helvetica, Arial, sans-serif;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  @media only screen and (min-width: 600px) {
+   margin-top: 60px; 
+  }
 }
 </style>
